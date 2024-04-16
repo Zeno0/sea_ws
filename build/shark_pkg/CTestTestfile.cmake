4ca897e9a7a69faacf3a5680db4b433eb01829c7
Shekhar Kumar <zatch216@gmail.com>
2024-04-16 21:42:59 +0530

    removed

--- build/shark_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shen/sea_ws/src/shark_pkg
-# Build directory: /home/shen/sea_ws/build/shark_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
